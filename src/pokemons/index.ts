7e36aee217636acf3f06dde37c032148d449f129
Roberto Núñez <robertonc1599@gmail.com>
2025-01-06 08:50:35 -0500

    Screen for a specific pokémon has been designed, also, there are some configurastions on next.config.ts

--- src/pokemons/index.ts
+++ src/pokemons/index.ts
@@ -1,3 +1,4 @@
 export { PokemonsGrid } from "./components/PokemonsGrid";
+export type { Pokemon } from "./interfaces/pokemon";
 export type { PokemonsResponse } from "./interfaces/pokemons-response";
 export type { SimplePokemon } from "./interfaces/simple-pokemon";
